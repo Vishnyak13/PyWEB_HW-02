1620e160ed388e64a1077c9130cba9f4ed653143
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2022-10-02 17:35:15 +0300

    Abstract Classes

--- abstractclasses.py
+++ abstractclasses.py
@@ -1,1 +1,96 @@
 from abc import ABC, abstractmethod
+from collections import UserDict
+
+
+class AbstractFields(ABC):
+    @abstractmethod
+    def __init__(self, value):
+        pass
+
+    @abstractmethod
+    @property
+    def value(self):
+        pass
+
+    @abstractmethod
+    @value.setter
+    def value(self, value):
+        pass
+
+
+class AbstractRecordContainer(UserDict, ABC):
+    @abstractmethod
+    def load_data(cls, filepath: str) -> None | dict:
+        pass
+
+    @abstractmethod
+    def backup_data(handler) -> None:
+        pass
+
+    @abstractmethod
+    def add_record(self, record) -> None:
+        pass
+
+    @abstractmethod
+    def remove_record(self, *args: str) -> str:
+        pass
+
+    @abstractmethod
+    def record_exists(self, record_name: str) -> bool:
+        pass
+
+    @abstractmethod
+    def show_records(self) -> str:
+        pass
+
+    @abstractmethod
+    def search_records(self, needle: str) -> str:
+        pass
+
+
+class AbstractNoteRecord(ABC):
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def change_title(self, new_title: str):
+        pass
+
+    @abstractmethod
+    def change_tags(self, *args: str):
+        pass
+
+    @abstractmethod
+    def change_text(self, new_text: str):
+        pass
+
+
+class AbstractAddressBookRecord(ABC):
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def add_phone(self, phone: str):
+        pass
+
+    @abstractmethod
+    def count_days_to_birthday(self) -> str:
+        pass
+
+    @abstractmethod
+    def add_birthday(self, birthday: str) -> None:
+        pass
+
+    @abstractmethod
+    def add_email(self, email: str) -> None:
+        pass
+
+    @abstractmethod
+    def add_address(self, *args: str) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    pass
